30f3b6f37ab2abcafa2968dfb607ac7171878a84
anthonyzivkovic-ops <anthony.zivkovic@gmail.com>
2026-06-30 19:26:53 +1200

    Update redisClient.js

--- frontend/src/redisClient.js
+++ frontend/src/redisClient.js
@@ -1,9 +1,6 @@
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-
-UPSTASH_REDIS_REST_URL="https://key-lemur-73046.upstash.io"
-UPSTASH_REDIS_REST_TOKEN="gQAAAAAAAR1WAAIgcDFjNTdmMzM3NzE3NDk0MTczYTdhMDg2Mjg2YjYxNjk2MA"
-
+  url: 'https://your-database-id.upstash.io',
+  token: 'your-secret-token-here',
 });
-I'm
